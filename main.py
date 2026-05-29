51abb6b7d3b7114d7ac54c9c2dc34d522797f537
Shin-sung-su <note0408@mju.ac.kr>
2026-05-29 05:56:44 +0000

    60231805-신성수-과제4

--- main.py
+++ main.py
@@ -9,19 +9,32 @@ name = ""
 def input_weather_info():
     global name
     name = input("사용자님의 이름을 입력하세요: ")
-    temp = float(input("현재 기온을 입력하세요(°C): "))
 
-    ex_temp = []
-    for i in range(3):
-        t = float(input(f"{i+1}시간 후 예상 기온을 입력하세요(°C): "))
-        ex_temp.append(t)
+    try: 
+        temp = float(input("현재 기온을 입력하세요(°C): "))
 
-    humidity = float(input("현재 습도를 입력하세요(%): "))
-    wind = float(input("현재 풍속을 입력하세요(Km/h): "))
-    rain_prob = int(input("강수 확률을 입력하세요(%): "))
-    dust = int(input("미세먼지 수치를 입력하세요(µg/m³): "))
+        ex_temp = []
+        days = ["오늘", "내일"]
+        times = ["오전", "오후"]
 
-    return [temp, ex_temp, humidity, wind, rain_prob, dust]
+        print("\n[향후 이틀간의 예상 기온을 입력합니다.]")
+        for d in days:
+            daily_temp = []
+            for t in times:
+                t_input = float(input(f"{d} {t} 예상 기온을 입력하세요(°C): "))
+                daily_temp.append(t_input)
+            ex_temp.append(daily_temp)
+
+        humidity = float(input("현재 습도를 입력하세요(%): "))
+        wind = float(input("현재 풍속을 입력하세요(Km/h): "))
+        rain_prob = int(input("강수 확률을 입력하세요(%): "))
+        dust = int(input("미세먼지 수치를 입력하세요(µg/m³): "))
+
+        return [temp, ex_temp, humidity, wind, rain_prob, dust]
+    
+    except ValueError:
+        print("/n [입력 오류] 숫자만 입력 가능합니다. 다시 시도해주세요.")
+        return []
 
 # 2. 불쾌지수 및 체감온도 계산 함수 #
 def check_index(temp, humidity, wind):
@@ -91,30 +104,63 @@ def check_data(data):
         return False
     return True 
 
-# 5. 메인 메뉴 함수
+# 5. 파일 저장 함수 #
+def save_report(data):
+    ex_temp = data[1]
+    days = ["오늘", "내일"]
+    times = ["오전", "오후"]
+
+    try:
+        with open("weather_report.txt", "w", encoding="utf-8") as f:
+            f.write(f"--- {name}님의 스마트 날씨 리포트 ---\n")
+            f.write(f"현재 기온: {data[0]}°C / 습도: {data[2]}%\n\n")
+            f.write("[예상 기온 상세 현황]\n")
+
+            for i in range(len(ex_temp)):
+                f.write(f"- {days[i]}")
+                for j in range(len(ex_temp[i])):
+                    f.write(f"{times[j]}({ex_temp[i][j]}°C)")
+                f.write("\n")
+
+            f.write(f"\n누적 외출 피로도 지수: {stress_index}\n")
+            f.write("-" * 45)
+        print("\n 'weather_report.txt'로 저장이 완료되었습니다.")
+    except Exception as e:
+        print(f"파일 저장 실패: {e}")
+
+# 6. 메인 메뉴 함수 #
 def main_menu():
     weather_data = []
     
     while True:
         print("\n----🌦️ 스마트 날씨 매니저 메뉴 ----")
         print("1. 날씨 정보 입력")
-        print("2. 오늘 기온 통계 보기")
+        print("2. 기온 상세 현황")
         print("3. 불쾌/체감 지수 확인 ")
         print("4. 준비물 및 스트레스 지수 확인")
-        print("5. 프로그램 종료")
+        print("5. 파일 저장")
+        print("6. 프로그램 종료")
 
         choice = input("-> 원하시는 메뉴 번호를 입력하세요: ")
         print("")
 
         if choice == '1':
             weather_data = input_weather_info()
-            print("\n정보가 저장되었습니다.")
-
+            if weather_data:
+                print("\n정보가 저장되었습니다.")
+    
         elif choice == '2':
             if check_data(weather_data):
                 ex_temp = weather_data[1]
-                print(f"향후 3시간 최고 기온: {max(ex_temp)}°C")
-                print(f"평균 온도: {sum(ex_temp)/len(ex_temp):.1f}°C")
+                days = ["오늘", "내일"]
+                times = ["오전", "오후"]
+
+                print("\n[향후 예상 기온 상세 현황]")
+                for i in range(len(ex_temp)):
+                    print(f"> {days[i]} 데이터:", end = " ")
+                    for j in range(len(ex_temp[i])):
+                        print(f"{times[j]} {ex_temp[i][j]}°C", end = " | ")
+                    print()
 
         elif choice == '3':
             if check_data(weather_data):
@@ -129,11 +175,15 @@ def main_menu():
                 print(f"현재 {name}님의 외출 피로도 지수는 {stress_index}입니다.")
         
         elif choice == '5':
+            if check_data(weather_data):
+                save_report(weather_data)
+
+        elif choice == '6':
             print("매니저를 종료합니다. 행복한 하루 되세요!")
             break
 
         else:
-            print("잘못된 입력입니다. 1~5번 사이를 선택해주세요.")
+            print("잘못된 입력입니다. 1~6번 사이를 선택해주세요.")
 
 # 프로그램 시작 #
 main_menu()
